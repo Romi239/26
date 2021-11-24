603b5a4ea6e1e5dcfe89f8818fcee8e7ed296158
Romy239 <rominamaximobadillo@gmail.com>
2021-11-23 18:47:56 -0600

    hola

---
+++ b.js
@@ -0,0 +1,14 @@
+/*function numero(a,b){
+    suma = a+b 
+   return suma;
+}
+console.log("el resultado es :" +numero(15,3));
+
+function suma(a,b,c,d,e){
+    suma = a+b+c+d+e
+    return suma;
+}
+console.log("el promedio es :" +suma(15,17,18,16,)*/
+
+let nombre = r => r;
+alert(nombre("romina"))
